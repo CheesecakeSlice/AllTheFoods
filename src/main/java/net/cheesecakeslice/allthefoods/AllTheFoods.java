5da01668b987f16fb5a624811fdc517460d3a57f
Joel <safmanjoel@gmail.com>
2024-06-09 20:13:55 +0200

    First item, toast, has been added

--- src/main/java/net/cheesecakeslice/allthefoods/AllTheFoods.java
+++ src/main/java/net/cheesecakeslice/allthefoods/AllTheFoods.java
@@ -1,5 +1,6 @@
 package net.cheesecakeslice.allthefoods;
 
+import net.cheesecakeslice.allthefoods.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class AllTheFoods implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/cheesecakeslice/allthefoods/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.cheesecakeslice.allthefoods.item;
+
+import net.cheesecakeslice.allthefoods.AllTheFoods;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item TOAST = registerItem("toast", new Item(new FabricItemSettings()));
+
+    private static void addItemToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(TOAST);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(AllTheFoods.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        AllTheFoods.LOGGER.info("Registering Mod Items For " + AllTheFoods.MOD_ID );
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemToIngredientItemGroup);
+    }
+}
